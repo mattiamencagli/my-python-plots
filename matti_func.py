d6cd8dae1bbd1bfa0e504f404ba40ba0111ff6b3
mattiamencagli <mattiamencagli@hotmail.it>
2022-05-12 13:59:26 +0200

    ..

--- matti_func.py
+++ matti_func.py
@@ -7,7 +7,7 @@ rad_to_deg = 180.0 / np.pi
 
 ################################################################################################################################
 ################################################################################################################################
-def open_reg(direc, name, jump=1):
+def open_reg(direc, name, jump=1, checkstop=False):
     ''' open the files in which I write the data of the refularized systems '''
     f = open(direc + name)
     file=f.readlines()
@@ -21,14 +21,16 @@ def open_reg(direc, name, jump=1):
     
     #check how many time the reg sys has reached the synch with its center of mass
     stop=[]
-    i=0
-    while i<len(file):
-        if(file[i]=="stop\n"):
-            stop.append(i)
-            file.pop(i)
-            print("STOP, i = %d"%i)
-        i += 1
-    stop.pop()
+    if(checkstop):
+        i=0
+        while i<len(file):
+            if(file[i]=="stop\n"):
+                stop.append(i)
+                file.pop(i)
+                print("STOP, i = %d"%i)
+            i += 1
+        if(len(stop)!=0):
+            stop.pop()
     
     #the data
     o = np.loadtxt(file)
@@ -60,14 +62,15 @@ def change_units(o, m, m_unit, t_unit, v_unit, conv_pc_UA):
 
 ################################################################################################################################
 ################################################################################################################################
-def kepl_units(a,inc,ome,Ome,nu,conv_pc_UA):
+def kepl_units(a,T,inc,ome,Ome,nu,t_unit,conv_pc_UA):
     ''' change the unit of measure '''
     a *= conv_pc_UA
+    T *= t_unit
     inc *= rad_to_deg
     ome *= rad_to_deg
     Ome *= rad_to_deg
     nu *= rad_to_deg
-    print("units: UA, deg")
+    print("units: UA, years, deg")
     return
 
 
@@ -82,10 +85,10 @@ def acos2(num, den, posflag):
     arccos[mask1] = np.arccos(cose[mask1])
     
     mask2 = mask1 & (posflag < 0.0)
-    arccos[mask2] = - arccos[mask2]
+    arccos[mask2] *= -1.0
     
     mask3 = cose <= -1.0
-    arccos[(~mask1) & mask3] = np.pi
+    arccos[mask3] = np.pi
 
     return arccos
 
@@ -103,24 +106,28 @@ def cart_to_kepl(m1, pos1, vel1, m2, pos2, vel2):
     vcirc2 = mu/r
 
     #semimajor axis
-    a = -mu/( v2 - 2.0*vcirc2 )
+    a = -mu / ( v2 - 2.0*vcirc2 )
+    
+    #period
+    T = 2 * np.pi * np.sqrt(np.abs(a*a*a)/(m1+m2))
 
     #angular momentum vector
-    lx = (dr[:,1]*dv[:,2] - dr[:,2]*dv[:,1])
-    ly = (dr[:,2]*dv[:,0] - dr[:,0]*dv[:,2])
-    lz = (dr[:,0]*dv[:,1] - dr[:,1]*dv[:,0])
+    lx = dr[:,1]*dv[:,2] - dr[:,2]*dv[:,1]
+    ly = dr[:,2]*dv[:,0] - dr[:,0]*dv[:,2]
+    lz = dr[:,0]*dv[:,1] - dr[:,1]*dv[:,0]
     l = np.sqrt ( lx*lx + ly*ly + lz*lz )
 
     vdiff2 = v2 - vcirc2
-    vr = (dr[:,0]*dv[:,0] + dr[:,1]*dv[:,1] + dr[:,2]*dv[:,2])/r
-    rvr = r*vr
+    rvr = (dr[:,0]*dv[:,0] + dr[:,1]*dv[:,1] + dr[:,2]*dv[:,2])
+    vr = rvr/r
     muinv = 1.0/mu
 
     #eccentricity
-    ex = vdiff2*dr[:,0] - rvr*dv[:,0]
-    ey = vdiff2*dr[:,1] - rvr*dv[:,1]
-    ez = vdiff2*dr[:,2] - rvr*dv[:,2]
+    ex = muinv * (vdiff2*dr[:,0] - rvr*dv[:,0])
+    ey = muinv * (vdiff2*dr[:,1] - rvr*dv[:,1])
+    ez = muinv * (vdiff2*dr[:,2] - rvr*dv[:,2])
     e = np.sqrt( ex*ex + ey*ey + ez*ez )
+#     e = np.sqrt(1 + (v2-2.0*vcirc2)*l*l/(mu*mu))
 
     #inclination
     inc = acos2(lz, l, np.ones_like(l))
@@ -144,8 +151,8 @@ def cart_to_kepl(m1, pos1, vel1, m2, pos2, vel2):
     mask2 = (inc < np.pi * 0.5)
     m_comb1 = mask1 & mask2
     m_comb2 = mask1 & (~mask2)    
-    m_comb3 = no_mask1 & mask2
-    m_comb4 = no_mask1 & (~mask2)
+#     m_comb3 = no_mask1 & mask2
+#     m_comb4 = no_mask1 & (~mask2)
     
     #planar case
     true_long[mask1] = acos2(dr[mask1,0], r[mask1], dr[mask1,1]) #true longitude (planar)
@@ -158,14 +165,15 @@ def cart_to_kepl(m1, pos1, vel1, m2, pos2, vel2):
     nu[m_comb2] = peri_long[m_comb2] - true_long[m_comb2] #true anomaly (planar, retrograde)
             
     #non-planar case
-    ome_nu[no_mask1] = acos2(nx[~mask1]*dr[no_mask1,0] + ny[no_mask1]*dr[no_mask1,1], n[no_mask1]*r[~mask1], dr[no_mask1,2])
+    ome_nu[no_mask1] = acos2(nx[no_mask1]*dr[no_mask1,0] + ny[no_mask1]*dr[no_mask1,1], n[no_mask1]*r[no_mask1], dr[no_mask1,2])
     ome[no_mask1] = acos2(nx[no_mask1]*ex[no_mask1] + ny[no_mask1]*ey[no_mask1], n[no_mask1]*e[no_mask1], ez[no_mask1])
     
-    nu[m_comb3] = ome_nu[m_comb3] - ome[m_comb3] #true anomaly (inclined, prograde)
-    
-    nu[m_comb4] = ome_nu[m_comb4] - ome[m_comb4] #true anomaly (inclined, retrograde)
+    nu[no_mask1] = ome_nu[no_mask1] - ome[no_mask1] #true anomaly
+    #qui trani fa operazioni extra, forse gli servono per i termini post newtoniani
+#     nu[m_comb3] = ome_nu[m_comb3] - ome[m_comb3] #true anomaly (inclined, prograde)
+#     nu[m_comb4] = ome_nu[m_comb4] - ome[m_comb4] #true anomaly (inclined, retrograde)
 
-    return a, e, inc, ome, Ome, nu
+    return a, T, e, inc, ome, Ome, nu
 
 
 ################################################################################################################################
